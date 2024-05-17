e18a314edf7aeca5b8c6999037bc1ec3ff721d2e
erfi-god <159731868+erfi-god@users.noreply.github.com>
2024-05-17 21:05:35 +0330

    Update prx.py

--- prx.py
+++ prx.py
@@ -17,23 +17,27 @@ def main():
     os.system('cls' if os.name == 'nt' else 'clear')
 
     # نمایش منو
-    print("منو:")
-    print("1. بازی")
-    print("2. خرید")
-    print("3. قدم زدن")
-
+    print("ErFi PrX")
+    print("1_account")
+    print("2_group")
+    print( "3_channel")
+    print( "4_revesh")
     # دریافت ورودی از کاربر
-    choice = input("لطفاً یک عدد را انتخاب کنید: ")
+    choice = input("PrX")
 
     # پاسخ به انتخاب کاربر
     if choice == '1':
-        print("شما بازی را انتخاب کردید.")
+        print(" (http://sharz.iran/openrap.svsov+Af+Af/link)(https://darkweblive.net/nyc-man-busted-for-peddling-coke-heroin-on-the-darkweb)(https://HACKDRES2/filtering/link-nofozi)(http://1.6.9.92.2/C.f.c.9.1.8.1.6.1_1_1_amir.dyson/1_80.20.10.73.104.410.210.00.8.19)(http://yun.ir/73wpsf)(https://s2.uupload.ir/files/1_rdq.png)(http://9igfgjjg9ijg9ifgj9uf.gigfa.com/yftt15k-sex-in-gif-Soroush-seranitor-kosnanat-rahbar.html)(https://cyberpolice.ir/node/163947)(http://TILik3.8.0.0.0.0.0.0.7.Sid.me/http://TILik3.8.0.0.0.0.0.0.7.Sid.me/http://TILik3.8.0.0.0.0.0.0.7)(http://68.3.6.000)(http://68.3.6.000)(https://s4.uupload.ir/files/img_20210728_194732_694_vlkn.jpg) ")
+        
     elif choice == '2':
-        print("شما خرید را انتخاب کردید.")
+        print(" http://dscript-sxs22-87-99-typer-filteri.gigfa.com/dxprit.html(Filter.Filter.Filter0This.group.sends.this.group.sexually.and.pornographic.(audio.video.photo.tag.GIF.text).to.this.group.with.violence.Please.block.it.as.soon.as.possible.Thanks.to.the.soroush.(V3.2.9/filter.anlain.filter.com.ir.)(0.4.9.2/f/h/g//6.1.2.0.9.3.7.4.8.5.1.6.4.7.3.1.0.2.5.6.7.2.8.1.0.4.4.8.)(https://uploadkon.ir/uploads/406010_23IMG-20230410-120825-073.jpg)(https://bit.ly/3ild93L)(https://tex.sexi.000757.fil.chat.anli.Xxx.porn.photo.tex.sexi.ey98.com.Xxx.fil.filter.chat.anlin.in.soroush.in.com)(In.diesem.Chat.werden.sexuelle.Inhalte.und.Beleidigungen.des.Obersten.Führers.der.Islamischen.Revolution.im.Iran.sowie.Beleidigungen.der.Nutzer.von.Soroush.Plus.ausgestrahlt.)(https://darkweb24.net/how-to-keep-you-safe-on-the-internet-3-tips-to-maximize-your-safeness)(http://140.82.121.3)(https://github.com/noob-hackers/efst)(https://0/7/1/5//report.chat//3 ")
     elif choice == '3':
-        print("شما قدم زدن را انتخاب کردید.")
+        print("1.6.9.92.2/C.f.c.9.1.8.1.6.1_1_1_amir.dyson/1_80.20.10.73.104.410.210.00.8.19./')4.0.1.4/f.i.h/gk/0/7/1/5//report.chat//3/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/04.5.3//5.2.5.7.2.6.8.4.2.4.7.9.3.1.5.2.7.9.8.0.1.34.7.1.7//h/d/y/i.5.2.2.5.7.8.5.2.2.6.7.9.6.4.2.2.7.5.2.2.4.6.4.2.1.45.6.4.2.4.6.8.9.8.7.5.3.2.4.6.4hspy24.ir/%D9%87%DA%A9-%D8%B1%D9%88%D8%A8%DB%8C%DA%A9%D8%A7-%D8%A8%D8%A7-%D8%A2%DB%8C%D8%AF%DB%8C-2022HACKDRES2/filtering/link-nofozi(2.3.0.4)s2.uupload.ir/files/1_rdq.pngyun.ir/73wpsfbit.ly/3ild93L(/rest_122334_filteri.com)Hsb_17gcyberpolice.ir/node/16394")
+    elif choice == '4':
+        print("به زودی ...")
+     
     else:
-        print("انتخاب نامعتبر!")
+        print("prx?")
 
 if __name__ == "__main__":
     # اجرای اصلی
